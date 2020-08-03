4d7b5f5bd7eee0de0796a391086a0c3d5ce2a708
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-04 00:25:00 +0700

    drawWorm function

--- game.py
+++ game.py
@@ -25,10 +25,20 @@ class Game():
       pygame.draw.line(self.screen, Config.DARKGRAY,
                        (0, y), (Config.WINDOW_WIDTH, y))
 
+  def drawWorm(self):
+    for coord in self.snake.wormCoords:
+      x = coord['x'] * Config.CELLSIZE
+      y = coord['y'] * Config.CELLSIZE
+      wormSegmentReact = pygame.Rect(x, y, Config.CELLSIZE, Config.CELLSIZE)
+      pygame.draw.rect(self.screen, Config.DARKGREEN, wormSegmentReact)
+      wormInnerSegmentRect = pygame.Rect(x + 4, y + 4, Config.CELLSIZE - 8, Config.CELLSIZE - 8)
+      pygame.draw.rect(self.screen, Config.GREEN, wormInnerSegmentRect)
+
   def draw(self):
     self.screen.fill(Config.BG_COLOR)
     # in here well draw snake, grid, apple, scroe
     self.drawGrid()
+    self.drawWorm()
     pygame.display.update()
     self.clock.tick(Config.FPS)
 
@@ -63,7 +73,7 @@ class Game():
         elif event.type == pygame.KEYDOWN:
           self.handleKeyEvents(event)
 
-    #   self.snake.update(self.apple)
+      self.snake.update(self.apple)
       self.draw()
-      # if self.isGameOver():
-      #     break
+      if self.isGameOver():
+        break
