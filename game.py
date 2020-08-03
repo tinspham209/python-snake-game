cea6d58de7cb8c31bcc0de6d1a0775a1db054292
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-03 23:09:35 +0700

    def __init__ method Game

---
+++ game.py
@@ -0,0 +1,16 @@
+from config import Config
+from snake import Snake
+from apple import Apple
+import pygame
+
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(
+            Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT)
+        self.clock = pygame.time.Clock()
+        self.BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+        pygame.display.set_caption('Snake')
+        self.apple = Apple()
+        self.snake = Snake()
