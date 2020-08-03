bea7b96bbe89cd4234582e4947dbf140dd409fe7
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-03 23:16:24 +0700

    Initilize game.py

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ class Game():
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(
-            Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT)
+            (Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
         self.BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
         pygame.display.set_caption('Snake')
