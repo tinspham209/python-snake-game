f1e3d6cf18c669475fed23eb5239b9885b3116b8
BHyeonKim <rlaqhguse@gmail.com>
2020-10-30 00:25:12 +0900

    코드 한글 주석 추가

--- apple.py
+++ apple.py
@@ -5,7 +5,7 @@ from config import Config
 class Apple():
   def __init__(self):
     self.setNewLocation()
-
+  # 사과 생성 메소드
   def setNewLocation(self):
-    self.x = random.randint(0, Config.CELLWIDTH - 1)
-    self.y = random.randint(0, Config.CELLHEIGHT - 1)
+    self.x = random.randint(0, Config.CELLWIDTH - 1) # 0부터 31사이에 난수생성 및 x에 저장
+    self.y = random.randint(0, Config.CELLHEIGHT - 1) # 0부터 23사이에 난수생성 및 y에 저장
