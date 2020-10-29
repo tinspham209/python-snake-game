c6385d1cabef034239aa4bd27e31c44027c7a3bb
BHyeonKim <rlaqhguse@gmail.com>
2020-10-30 00:19:13 +0900

    코드 한글 주석 추가

--- snake.py
+++ snake.py
@@ -9,33 +9,37 @@ class Snake():
   RIGHT = 'right'
   HEAD = 0
 
+  #snake 객체 생성자
   def __init__(self):
-    self.x = random.randint(5, Config.CELLWIDTH - 6)
-    self.y = random.randint(5, Config.CELLHEIGHT - 6)
-    self.direction = self.RIGHT
-    self.wormCoords = [
-        {'x': self.x, 'y': self.y},
-        {'x': self.x - 1, 'y': self.y},
-        {'x': self.x - 2, 'y': self.y}
+    #snake의 위치 랜덤 생성    가로 32픽셀 세로 24픽셀
+    self.x = random.randint(5, Config.CELLWIDTH - 6)  # 5부터 28까지 난수생성 후 x좌표에 저장
+    self.y = random.randint(5, Config.CELLHEIGHT - 6) # 5부터 18까지 난수생성 후 y좌표에 저장
+    self.direction = self.RIGHT # 시작시 무조건 오른쪽으로 가도록 설정
+    self.wormCoords = [ # 처음 뱀의 몸통
+        {'x': self.x, 'y': self.y}, # 머리
+        {'x': self.x - 1, 'y': self.y}, # 몸통
+        {'x': self.x - 2, 'y': self.y} # 꼬리
     ]
 
   def update(self, apple):
-    # check if snake has eaten an apply
-    if self.wormCoords[self.HEAD]['x'] == apple.x and self.wormCoords[self.HEAD]['y'] == apple.y:
-      apple.setNewLocation()
+    # check if snake has eaten an apply 뱀의 위치 업데이트 메소드
+    if self.wormCoords[self.HEAD]['x'] == apple.x and self.wormCoords[self.HEAD]['y'] == apple.y:   #뱀의 머리의 x좌표와 y좌표와 사과의 x좌표와 y좌표가 같을 경우, 즉 머리가 사과를 먹었을 경우
+      apple.setNewLocation()    # 사과를 새로운 장소에 생성
     else:
-      del self.wormCoords[-1]  # remove worms tail segment
+      del self.wormCoords[-1]  # remove worms tail segment # 뱀의 꼬리부분을 삭제
+
     # move the worm by adding a segment in the direction it is moving
-    if self.direction == self.UP:
+    #뱀의 움직임을 구현
+    if self.direction == self.UP: # 키보드 위쪽을 눌렀을 시 원래머리 위쪽에 새로운 머리를 생성
       newHead = {'x': self.wormCoords[self.HEAD]['x'],
                  'y': self.wormCoords[self.HEAD]['y'] - 1}
-    elif self.direction == self.DOWN:
+    elif self.direction == self.DOWN: # 키보드 아래쪽을 눌렀을 시 원래머리 아래에 새로운 머리를 생성
       newHead = {'x': self.wormCoords[self.HEAD]['x'],
                  'y': self.wormCoords[self.HEAD]['y'] + 1}
-    elif self.direction == self.LEFT:
+    elif self.direction == self.LEFT: # 키보드 왼쪽을 눌렀을 시 원래머리 왼쪽에 새로운 머리를 생성
       newHead = {'x': self.wormCoords[self.HEAD]['x'] - 1,
                  'y': self.wormCoords[self.HEAD]['y']}
-    elif self.direction == self.RIGHT:
+    elif self.direction == self.RIGHT: # 키보드 오른쪽을 눌렀을 시 원래머리 오른쪽에 새로운 머리를 생성
       newHead = {'x': self.wormCoords[self.HEAD]['x'] + 1,
                  'y': self.wormCoords[self.HEAD]['y']}
-    self.wormCoords.insert(0, newHead)
+    self.wormCoords.insert(0, newHead) # 뱀의 몸통 리스트 첫번째에 새로운 머리를 insert
